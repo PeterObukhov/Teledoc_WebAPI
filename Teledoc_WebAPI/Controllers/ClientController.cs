5e140df317c8730c415e175011224433ccd97bb0
Петр Обухов <petr.obuhov.2002@mail.ru>
2024-05-31 09:33:16 +0300

    separated controllers

--- Teledoc_WebAPI/Controllers/ClientController.cs
+++ Teledoc_WebAPI/Controllers/ClientController.cs
@@ -1,25 +1,33 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Teledoc_WebAPI.Tables;
 
 namespace Teledoc_WebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class TeledocDbController : ControllerBase
+    public class ClientController : ControllerBase
     {
         IDbActions dbActions;
-        public TeledocDbController(IDbActions dbActions)
+        public ClientController(IDbActions dbActions)
         {
             this.dbActions = dbActions;
         }
 
+        /// <summary>
+        /// Просмотр всех клиентов
+        /// </summary>
+        /// <returns>Перечисление клиентов</returns>
         [HttpGet(Name = "GetAllClients")]
         public IEnumerable<Client> GetClients()
         {
             return dbActions.GetClients();
         }
 
+        /// <summary>
+        /// Создание клиента
+        /// </summary>
+        /// <param name="client">Объект нового клиента</param>
+        /// <returns>Результат выполнения</returns>
         [HttpPost]
         [Route("CreateClient")]
         public IActionResult CreateClient([FromBody] Client client)
@@ -32,6 +40,12 @@ namespace Teledoc_WebAPI.Controllers
             return Ok();
         }
 
+        /// <summary>
+        /// Обновить данные клиента
+        /// </summary>
+        /// <param name="Id">id обновляемого клиента</param>
+        /// <param name="updatedClient">объект нового клиента</param>
+        /// <returns></returns>
         [HttpPut]
         [Route("UpdateClient")]
         public IActionResult UpdateClient(int Id, [FromBody] Client updatedClient)
@@ -51,6 +65,11 @@ namespace Teledoc_WebAPI.Controllers
             return Ok();
         }
 
+        /// <summary>
+        /// Удаление клиента
+        /// </summary>
+        /// <param name="Id">id удаляемого клиента</param>
+        /// <returns>Результат выполнения</returns>
         [HttpDelete]
         [Route("DeleteClient")]
         public IActionResult DeleteClient(int Id)
@@ -63,51 +82,6 @@ namespace Teledoc_WebAPI.Controllers
             }
 
             return new ObjectResult(deletedClient);
-        }
-
-        [HttpPost]
-        [Route("CreateFounder")]
-        public IActionResult CreateFounder([FromBody] Founder founder)
-        {
-            if (founder == null)
-            {
-                return BadRequest();
-            }
-            dbActions.CreateFounder(founder);
-            return Ok();
-        }
-
-        [HttpPut]
-        [Route("UpdateFounder")]
-        public IActionResult UpdateFounder(int Id, [FromBody] Founder updatedFounder)
-        {
-            if (updatedFounder == null || updatedFounder.Id != Id)
-            {
-                return BadRequest();
-            }
-
-            var client = dbActions.GetFounder(Id);
-            if (client == null)
-            {
-                return NotFound();
-            }
-
-            dbActions.UpdateFounder(updatedFounder);
-            return Ok();
-        }
-
-        [HttpDelete]
-        [Route("DeleteFounder")]
-        public IActionResult DeleteFounder(int Id)
-        {
-            var deletedFounder = dbActions.DeleteFounder(Id);
-
-            if (deletedFounder == null)
-            {
-                return BadRequest();
-            }
-
-            return new ObjectResult(deletedFounder);
         }
     }
 }
---
+++ Teledoc_WebAPI/Controllers/FounderController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Teledoc_WebAPI.Tables;
+
+namespace Teledoc_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FounderController : ControllerBase
+    {
+        IDbActions dbActions;
+        public FounderController(IDbActions dbActions)
+        {
+            this.dbActions = dbActions;
+        }
+
+        /// <summary>
+        /// Создание нового учредителя
+        /// </summary>
+        /// <param name="founder">Объект нового учредителя</param>
+        /// <returns>Результат выполнения</returns>
+        [HttpPost]
+        [Route("CreateFounder")]
+        public IActionResult CreateFounder([FromBody] Founder founder)
+        {
+            if (founder == null)
+            {
+                return BadRequest();
+            }
+            dbActions.CreateFounder(founder);
+            return Ok();
+        }
+        
+        /// <summary>
+        /// Обновление данных учредителя
+        /// </summary>
+        /// <param name="Id">id обновляемого учредителя</param>
+        /// <param name="updatedFounder">Объект нового учредителя</param>
+        /// <returns></returns>
+        [HttpPut]
+        [Route("UpdateFounder")]
+        public IActionResult UpdateFounder(int Id, [FromBody] Founder updatedFounder)
+        {
+            if (updatedFounder == null || updatedFounder.Id != Id)
+            {
+                return BadRequest();
+            }
+
+            var client = dbActions.GetFounder(Id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            dbActions.UpdateFounder(updatedFounder);
+            return Ok();
+        }
+
+        /// <summary>
+        /// Удаление учредителя
+        /// </summary>
+        /// <param name="Id">id удаляемого учредителя</param>
+        /// <returns></returns>
+        [HttpDelete]
+        [Route("DeleteFounder")]
+        public IActionResult DeleteFounder(int Id)
+        {
+            var deletedFounder = dbActions.DeleteFounder(Id);
+
+            if (deletedFounder == null)
+            {
+                return BadRequest();
+            }
+
+            return new ObjectResult(deletedFounder);
+        }
+    }
+}
+
